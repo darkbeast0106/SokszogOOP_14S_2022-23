cf35bc560556dd2095febfc9745241ccd87e0048
Budaházi Bence <budahazi.bence@petrik.hu>
2022-10-06 16:07:54 +0200

    Téglalap osztály tesztelése

--- src/hu/petrik/sokszogoop/Main.java
+++ src/hu/petrik/sokszogoop/Main.java
@@ -2,6 +2,11 @@ package hu.petrik.sokszogoop;
 
 public class Main {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 3; i++) {
+            double a = Math.random() * 5 + 10;
+            double b = Math.random() * 5 + 10;
+            Teglalap t = new Teglalap(a, b);
+            System.out.println(t);
+        }
     }
 }
